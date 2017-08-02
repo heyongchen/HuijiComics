9c7158e4dcbfbdbbd90d987c59ac49590796606c
HeYongchen <470604693@qq.com>
2017-08-02 15:24:33 +0800

    关于页面增加手动检查更新功能；

--- app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/AboutActivity.java
+++ app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/AboutActivity.java
@@ -1,14 +1,23 @@
 package com.huiji.comic.bobcat.huijicomics.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.huiji.comic.bobcat.huijicomics.R;
 import com.huiji.comic.bobcat.huijicomics.base.BaseActivity;
+import com.huiji.comic.bobcat.huijicomics.base.manager.PermissionManager;
 import com.huiji.comic.bobcat.huijicomics.utils.AppUtils;
+import com.pgyersdk.javabean.AppBean;
+import com.pgyersdk.update.PgyUpdateManager;
+import com.pgyersdk.update.UpdateManagerListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +30,10 @@ public class AboutActivity extends BaseActivity {
     ImageView ivAboutUs;
     @BindView(R.id.tv_about_us)
     TextView tvAboutUs;
+    @BindView(R.id.tv_version)
+    TextView tvVersion;
+    @BindView(R.id.ll_version)
+    LinearLayout llVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +49,47 @@ public class AboutActivity extends BaseActivity {
             }
         });
 
-        tvAboutUs.setText("Version：#" + AppUtils.getVersionName(this) + "\n本APP为个人开发\n估计也没人拿去搞什么商业用途了\n纯粹因为喜欢灰机汉化组的漫画而制作的APP");
+        tvVersion.setText(String.format("Version：#%s", AppUtils.getVersionName(this)));
+        llVersion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PgyUpdateManager.register(AboutActivity.this, null, new UpdateManagerListener() {
+
+                    @Override
+                    public void onUpdateAvailable(final String result) {
+                        // 将新版本信息封装到AppBean中
+                        final AppBean appBean = getAppBeanFromString(result);
+                        new AlertDialog.Builder(AboutActivity.this)
+                                .setTitle("版本更新")
+                                .setMessage("发现新版本，是否进行更新？")
+                                .setPositiveButton("开始更新", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        PermissionManager.requestPermission(AboutActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, new PermissionManager.OnPermissionCallback() {
+                                            @Override
+                                            public void onGranted() {
+                                                //开始下载
+                                                startDownloadTask(AboutActivity.this, appBean.getDownloadURL());
+                                            }
+
+                                            @Override
+                                            public void onDenied() {
+                                                PermissionManager.showAdvice(AboutActivity.this);
+                                            }
+                                        });
+                                    }
+                                })
+                                .setNegativeButton("暂不更新", null)
+                                .show();
+                    }
+
+                    @Override
+                    public void onNoUpdateAvailable() {
+                        Toast.makeText(AboutActivity.this, "当前已是最新版", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
     }
 
