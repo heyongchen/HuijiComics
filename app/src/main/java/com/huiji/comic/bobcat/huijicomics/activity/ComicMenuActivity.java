05f3be6f260f9d99e4cbfd5e95f0541c2220c522
HeYongchen <470604693@qq.com>
2017-08-07 10:22:19 +0800

    删除多余引用

--- app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/ComicMenuActivity.java
+++ app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/ComicMenuActivity.java
@@ -1,7 +1,6 @@
 package com.huiji.comic.bobcat.huijicomics.activity;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
