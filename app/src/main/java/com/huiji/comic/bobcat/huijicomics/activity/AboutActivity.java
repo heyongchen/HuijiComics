11b77f73c3f6b0377f23f3d272f17e98e0c3bd0a
HeYongchen <470604693@qq.com>
2017-08-17 12:09:17 +0800

    增加更新日志

--- app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/AboutActivity.java
+++ app/src/main/java/com/huiji/comic/bobcat/huijicomics/activity/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.huiji.comic.bobcat.huijicomics.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -9,8 +13,11 @@ import android.widget.TextView;
 
 import com.huiji.comic.bobcat.huijicomics.R;
 import com.huiji.comic.bobcat.huijicomics.base.BaseActivity;
+import com.huiji.comic.bobcat.huijicomics.bean.ComicListBean;
 import com.huiji.comic.bobcat.huijicomics.utils.AppUtils;
+import com.huiji.comic.bobcat.huijicomics.utils.UrlUtils;
 import com.huiji.comic.bobcat.huijicomics.utils.updateUtil.UpdateUtil;
+import com.huiji.comic.bobcat.huijicomics.widget.AddComicDialog;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -50,6 +57,24 @@ public class AboutActivity extends BaseActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_update_note) {
+            Intent intent = new Intent(AboutActivity.this, UpdateNoteActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
