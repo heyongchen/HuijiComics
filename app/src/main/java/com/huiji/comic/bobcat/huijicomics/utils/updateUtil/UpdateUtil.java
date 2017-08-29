a76f50ee538696604ff296e954f4c16360714293
HeYongchen <470604693@qq.com>
2017-08-29 11:25:18 +0800

    应用内更新换为调启系统下载管理器

--- app/src/main/java/com/huiji/comic/bobcat/huijicomics/utils/updateUtil/UpdateUtil.java
+++ app/src/main/java/com/huiji/comic/bobcat/huijicomics/utils/updateUtil/UpdateUtil.java
@@ -2,16 +2,28 @@ package com.huiji.comic.bobcat.huijicomics.utils.updateUtil;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 import android.widget.Toast;
 
 import com.huiji.comic.bobcat.huijicomics.R;
 import com.huiji.comic.bobcat.huijicomics.base.manager.PermissionManager;
 import com.huiji.comic.bobcat.huijicomics.utils.AppUtils;
+import com.huiji.comic.bobcat.huijicomics.utils.PathUtil;
 import com.huiji.comic.bobcat.huijicomics.utils.SPHelper;
 import com.huiji.comic.bobcat.huijicomics.utils.SpKey;
+import com.huiji.comic.bobcat.huijicomics.widget.ConfirmDialog;
 import com.pgyersdk.javabean.AppBean;
 import com.pgyersdk.update.PgyUpdateManager;
 import com.pgyersdk.update.UpdateManagerListener;
+
+import java.io.File;
 
 /**
  * 更新工具类
@@ -46,18 +58,42 @@ public class UpdateUtil {
                         @Override
                         public void onGranted() {
                             //开始下载
-                            new UpdateManager(context, appBean.getVersionCode(), false, appBean.getDownloadURL(), apkName, title, message, buttonCancel, buttonOk,
-                                    new UpdateManager.UpdateAppResultListener() {
+                            new ConfirmDialog(context, true)
+                                    .setTitle(title)
+                                    .setMessage(message)
+                                    .setButtonCancelText(buttonCancel)
+                                    .setButtonOKText(buttonOk)
+                                    .setOnConfirmListener(new ConfirmDialog.OnConfirmListener() {
                                         @Override
-                                        public void updateToNext() {
-                                            UpdateManagerListener.updateLocalBuildNumber(result);
+                                        public void onIgnore(boolean ignore) {
+                                            if (ignore) {
+                                                SPHelper.get().put(SpKey.IGNORE_VERSION, appBean.getVersionCode());
+                                            }
                                         }
 
                                         @Override
-                                        public void updateFailed(boolean forceUpdate) {
-
+                                        public void onCancel() {
                                         }
-                                    });
+
+                                        @Override
+                                        public void onOK() {
+                                            //下载新版本
+                                            download(context, appBean.getDownloadURL());
+                                        }
+                                    })
+                                    .show();
+//                            new UpdateManager(context, appBean.getVersionCode(), false, appBean.getDownloadURL(), apkName, title, message, buttonCancel, buttonOk,
+//                                    new UpdateManager.UpdateAppResultListener() {
+//                                        @Override
+//                                        public void updateToNext() {
+//                                            UpdateManagerListener.updateLocalBuildNumber(result);
+//                                        }
+//
+//                                        @Override
+//                                        public void updateFailed(boolean forceUpdate) {
+//
+//                                        }
+//                                    });
                         }
 
                         @Override
@@ -76,5 +112,56 @@ public class UpdateUtil {
             }
         });
 
+    }
+
+    /**
+     * 下载更新
+     *
+     * @param url
+     */
+    private static void download(Context context, String url) {
+        final String localUrl = PathUtil.getDownloadPath() + "/" + AppUtils.getAppName() + ".apk";
+        final File path = new File(localUrl);
+        if (path.exists()) {
+            path.delete();
+        }
+        final DownloadManager dManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle(AppUtils.getAppName());
+        // 设置下载路径和文件名
+        request.setDestinationUri(Uri.fromFile(path));
+        request.setDescription("正在更新");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setMimeType("application/vnd.android.package-archive");
+        // 设置为可被媒体扫描器找到
+        request.allowScanningByMediaScanner();
+        // 设置为可见和可管理
+        request.setVisibleInDownloadsUi(true);
+        // 获取此次下载的ID
+        final long refernece = dManager.enqueue(request);
+        Toast.makeText(context, "开始下载", Toast.LENGTH_SHORT).show();
+        // 注册广播接收器，当下载完成时自动安装
+        final IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                long myDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                if (refernece == myDownloadId) {
+                    Toast.makeText(context, "下载完成", Toast.LENGTH_SHORT).show();
+                    Intent install = new Intent(Intent.ACTION_VIEW);
+                    install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //兼容7.0私有文件权限
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".fileProvider", path);
+                        install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        install.setDataAndType(apkUri, "application/vnd.android.package-archive");
+                    } else {
+                        install.setDataAndType(Uri.fromFile(path), "application/vnd.android.package-archive");
+                    }
+                    context.startActivity(install);
+                }
+            }
+        };
+        context.registerReceiver(receiver, filter);
     }
 }
